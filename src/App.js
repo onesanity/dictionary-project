57789a67f6b885a36e49e0a1d8fd8d23d708964c
onesanity <98060550+onesanity@users.noreply.github.com>
2022-07-03 13:29:28 +0200

    created Search Engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import logo from './logo.png';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img 
         src={logo} 
         className="App-logo img-fluid" 
         alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className='text-center'>
+        <small>Coded by Alina Nguyen</small>
+
+        </footer>
+      </div>
     </div>
   );
 }
