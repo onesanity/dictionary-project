83583b290320655e31c8fd014043a6635dbbff90
onesanity <98060550+onesanity@users.noreply.github.com>
2022-07-03 15:58:14 +0200

    Rendered the results and meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
 import axios from "axios";
-import './Dictionary.css';
+import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response){
-    console.log(response.data[0]);
-
+    setResults(response.data[0]);
   }
 
   function search(event){
     event.preventDefault();
     
     // documentation: https://dictionaryapi.dev/
-    
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
-
   
   function handleKeywordChange(event){
     setKeyword(event.target.value);
@@ -30,7 +29,8 @@ export default function Dictionary() {
         <input type="search" onChange=
         {handleKeywordChange} />
       </form>
+      <Results results={results} />
 
     </div>
-  )
+  );
 }
